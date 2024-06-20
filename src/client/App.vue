f54f5bc1e954bfea2160453f328698270a5f2ada
Roberto Illesca <robertillesca1@gmail.com>
2024-06-20 18:07:13 -0400

    implementacion navbar

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,3 +1,19 @@
 <template>
+  <Navbar v-if="isAuthenticated"/>
   <RouterView />
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  },
+  computed: {
+    isAuthenticated() {
+      return true;
+    }
+  }
+}
+</script>
