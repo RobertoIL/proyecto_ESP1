2e67d07fe19907c38064cdc17ab8c3ff60e82029
Roberto Illesca <robertillesca1@gmail.com>
2024-06-23 23:12:07 -0400

    config mongo y conexion

---
+++ src/server/config/environment.js
@@ -0,0 +1,4 @@
+import dotenv from "dotenv";
+dotenv.config();
+const MONGO_URI = procces.env.MONGO_URI;
+export { MONGO_URI };
---
+++ src/server/config/mongo.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "./environment.js";
+
+async function connectDB() {
+  console.log("Trying to connect to database...");
+  try {
+    const connection = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to database");
+    return connection.connection.db; // Retornar la referencia de la base de datos
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(-1);
+  }
+}
+
+export default connectDB;
