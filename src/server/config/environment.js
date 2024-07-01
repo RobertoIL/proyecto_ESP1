0e4568a4d814fa804f2b2eefdd75afcf52f25574
Roberto Illesca <robertillesca1@gmail.com>
2024-07-01 17:27:06 -0400

    Change: jwt secret agregado

--- src/server/config/environment.js
+++ src/server/config/environment.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
-export { MONGO_URI };
+const JWT_SECRET = process.env.JWT_SECRET;
+export { MONGO_URI, JWT_SECRET };
