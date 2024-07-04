9e0afc227ad52c07d3a1248f95b3b9b0d2ad987b
Roberto Illesca <robertillesca1@gmail.com>
2024-07-03 23:00:39 -0400

    Add: componente footer

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Navbar v-if="isAuthenticated"/>
-  <RouterView />
+  <RouterView class="min-h-screen" />
+  <Footer />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   computed: {
     isAuthenticated() {
---
+++ src/client/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+<footer class="bg-gray-900 p-4 items-center text-center">
+    <span class="text-sm text-center text-gray-200">© 2024 <a href="" class="hover:underline">MiniJuegos™</a>. All Rights Reserved.
+    </span>
+</footer>
+</template>
