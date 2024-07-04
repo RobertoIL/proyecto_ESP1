20f685b5c02032885875f67bf58f6d10e09a147e
Roberto Illesca <robertillesca1@gmail.com>
2024-07-03 23:00:50 -0400

    Change: nuevo estilo navbar

--- src/client/components/Navbar.vue
+++ src/client/components/Navbar.vue
@@ -1,40 +1,27 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <RouterLink to="/">
-                WebApp
-            </RouterLink>
-            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <RouterLink to="/">
-                        <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                    </RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="">
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                    </RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/game">
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Game</a>
-                    </RouterLink>
-                </li>
-            </ul>
-            </div>
+  <nav>
+    <div class="bg-gray-800 text-slate-100">
+      <div class="flex justify-between h-16 px-10 shadow items-center">
+        <div class="flex items-center space-x-8">
+          <RouterLink to="/" class="text-xl lg:text-2xl font-bold cursor-pointer hover:text-yellow-300">MiniJuegos</RouterLink>
+          <div class="hidden md:flex justify-around space-x-4">
+            <RouterLink to="/" class="hover:text-indigo-600">Home</RouterLink>
+            <RouterLink to="/game" class="hover:text-indigo-600">Game</RouterLink>
+            <RouterLink to="/historial" class="hover:text-indigo-600">Historial</RouterLink>
+            <RouterLink to="/wiki" class="hover:text-indigo-600">Wiki</RouterLink>
+          </div>
         </div>
-    </nav>
+        <div class="flex space-x-4 items-center">
+          <RouterLink to="/login" class="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">Login</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 import { RouterLink } from "vue-router";
+
 export default {
     name: 'Navbar',
     components: {
