09218151c8d343b631f761f4f71c8d8cad97c7f9
Roberto Illesca <robertillesca1@gmail.com>
2024-07-03 22:58:52 -0400

    Change: nuevo estilo login y register

--- src/client/views/HomeView.vue
+++ src/client/views/HomeView.vue
@@ -1,1 +1,41 @@
-<template></template>
+<template>
+    <main class="flex flex-col items-center bg-home_background bg-cover">
+        <div class="px-6 pt-10 lg:px-8 bg-gray-800 bg-opacity-90 w-full">
+            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                <div class="text-center">
+                    <h1 class="text-5xl font-extrabold font-font_tittle tracking-tight text-white sm:text-7xl drop-shadow-lg">
+                        Disfruta jugando sin límites
+                    </h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-300 opacity-90">
+                        ¡Podrás seleccionar al personaje que gustes para iniciar la partida!
+                    </p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <RouterLink to="/game">
+                            <button class="px-6 py-3 bg-blue-600 rounded-full text-white font-bold shadow-lg hover:bg-blue-800 transition transform hover:-translate-y-1 hover:scale-110 duration-300">
+                                JUGAR
+                            </button>
+                        </RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-gray-800 w-full pb-24 py-10 flex items-center justify-center">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                        <dd class="order-first text-4xl font-semibold tracking-tight text-white sm:text-5xl drop-shadow-lg">44 million</dd>
+                        <dt class="text-base leading-7 text-gray-300">Transacciones cada 24 horas</dt>
+                    </div>
+                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                        <dd class="order-first text-4xl font-semibold tracking-tight text-white sm:text-5xl drop-shadow-lg">$119,000</dd>
+                        <dt class="text-base leading-7 text-gray-300">Inversión</dt>
+                    </div>
+                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                        <dd class="order-first text-4xl font-semibold tracking-tight text-white sm:text-5xl drop-shadow-lg">46,000</dd>
+                        <dt class="text-base leading-7 text-gray-300">Nuevos usuarios</dt>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </main>
+</template>
--- src/client/views/LoginView.vue
+++ src/client/views/LoginView.vue
@@ -1,34 +1,49 @@
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Iniciar sesión
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required="">
-                        </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Olvide mi Contraseña</a>
-                        </div>
-                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            No tienes cuenta 
-                            <RouterLink to="/register">
-                                <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Registrase</a>
-                            </RouterLink>
-                        </p>
-                    </form>
-                </div>
-            </div>
+<main class="bg-login-background bg-cover">
+    <div class="flex-grow p-8 bg-gray-800 bg-opacity-90 h-screen">
+    <div class="p-8 bg-slate-200 bg-opacity-50 mx-[500px] rounded-xl">
+        <p class="text-3xl text-center font-bold pb-8">Iniciar Sesión</p>
+        <form class="max-w-sm mx-auto" @submit.prevent="login">
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" v-model="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
         </div>
-    </section>
-
+        <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+            <input type="password" id="password" v-model="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        </div>
+    <div class="flex flex-col space-y-4">
+        <RouterLink class="hover:text-blue-800 underline" to="/register">No tengo cuenta</RouterLink>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesión</button>
+    </div>
+        </form>
+    </div>
+</div>
+</main>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await axios.post('http://localhost:3000/auth/login', {
+                    email: this.email,
+                    password: this.password
+                });
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }
+}
+</script>
