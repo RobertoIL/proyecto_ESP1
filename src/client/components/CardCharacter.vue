72c046b3f3116ad868ae550c112b45b3fcd5d0dc
Bastian0089 <165496100+Bastian-Lagos@users.noreply.github.com>
2024-07-05 12:00:24 -0400

    feat: Se agrego informacion a la wiki
    Se agregaron pequeñas descripciones de cada personaje, asi como una imagen de estos. Para "archer" y  "lancer", debido a que su imagen individual era muy grande, se creo otra imagen de estos para no usar mucho espacio de mas en la wiki.

---
+++ src/client/components/CardCharacter.vue
@@ -0,0 +1,17 @@
+<template>
+<main class="bg-slate-800 text-slate-200 p-5 rounded-xl">
+    <img :src="image" alt="">
+    <p class="font-bold text-xl">{{ name }}</p>
+    <p class="text-justify">{{ description }}</p>
+</main>
+</template>
+
+<script>
+export default {
+    props: {
+        image: String,
+        name: String,
+        description: String
+    }
+}
+</script>
--- src/client/views/WikiView.vue
+++ src/client/views/WikiView.vue
@@ -1,5 +1,46 @@
 <template>
-    <main class="bg-gray-900 text-slate-200">
-        
+    <main class="flex flex-col items-center bg-home_background">
+        <div class="flex flex-col py-10 space-y-5 w-[600px]">
+                <CardCharacter
+                v-for="(character, index) in characters"
+                :key="index"
+                :image="character.image"
+                :name="character.name"
+                :description="character.description"/>
+            </div>
     </main>
+
 </template>
+<script>
+import CardCharacter from "../components/CardCharacter.vue";
+
+export default {
+    components: {
+        CardCharacter
+    },
+    data() {
+         return {
+             characters: [
+                {
+                 image: '/public/Archer/Archer_Wiki.png', 
+                 name: 'Arquero', 
+                 description: 'Elimina enemigos a distancia con precisión milimétrica, evade ataques con agilidad felina y se mueve sigilosamente para ataques furtivos.' 
+                }, 
+                { 
+                    image: '/public/Mage/Mage1.png', 
+                    name: 'Mago', 
+                    description: 'Controla el campo de batalla con ataques elementales, ilusiones y encantamientos, y posee un amplio conocimiento de hechizos.' }, 
+                { 
+                    image: '/public/Lancer/Lancer_Wiki.png', 
+                    name: 'Lancero', 
+                    description: 'Guerrero de primera línea que inflige gran daño, defiende con precisión mortal y lidera con lealtad e inspiración.' },
+                {
+                    image: '/public/Saber/Saber1.png',
+                    name: 'Espadachín',
+                    description: 'Elimina enemigos rápidamente con ataques letales de katana, se adapta a diferentes estilos de combate con otras armas y evade ataques con movimientos rápidos.'
+                },
+            ]
+        }
+    }
+}
+</script>
